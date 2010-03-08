cd17c7f5292af35805043730202579213b1fe0c2
Leif Warner <abimelech@gmail.com>
2010-03-08 03:16:56 -0800

    More info to Band index listings.

--- src/main/scala/com/portlanddatasystems/musicmap/MusicMap.scala
+++ src/main/scala/com/portlanddatasystems/musicmap/MusicMap.scala
@@ -10,26 +10,41 @@ class MusicMap extends Step with UrlSupport {
   val url = "http://example.org/"
   implicit val model = new Model
  
+  // Helper functions:
+
+  // Select all things of a given RDF:type.
   def allOf(category:Res) = Sparql selectAllX asRes where( (X, RDF.Type, category) ) from model
+
+  // Put a <?xsl-stylesheet ?> processing-instruction at the top of the response.
   def template(content:NodeSeq):NodeSeq = ProcInstr("xml-stylesheet", "type='text/xsl' href='edit.xsl'") ++ Text("\n") ++ content
 
   before {
     contentType = "application/xml"
   }
-  override def init = this.model.read("file:///home/leif/Projects/MusicMap/WebOutput/band.ttl", "TURTLE")
+  override def init = this.model.read("file:///home/leif/band.ttl", "TURTLE")
 
   get("/:type/:member") {
+    <span>{Res(url + params(":type") + "/" + params(":member"))/Foaf.givenname/asString}
     <ul>
       <li>Type: {params(":type")}</li>
       <li>Member: {params(":member")}</li>
     </ul>
+  </span>
   }
 
   // Display all the bands in the system.
   get("/bands") {
-    <bands>
-      {for (band <- allOf(Mo.MusicGroup) ) yield <band>{band}</band>}
-    </bands>
+    <bands>{
+      for (band <- allOf(Mo.MusicGroup) ) yield 
+        <band ref={band.uri}>
+          <name>{band/Foaf.name}</name>
+          <members>{
+            for (member <- band/performer) yield
+            <member>{
+            }</member>
+          }</members>
+        </band>
+    }</bands>
   }
 
   // Display all the people in the system.
