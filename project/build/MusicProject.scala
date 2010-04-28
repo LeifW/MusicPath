9cfabc0577357e721f743749d1ce0acd97116d8f
Leif Warner <abimelech@gmail.com>
2010-04-27 23:27:52 -0700

    Updated to use Scardf built by SBT rather than Maven.

--- project/build/MusicProject.scala
+++ project/build/MusicProject.scala
@@ -3,14 +3,12 @@ import sbt._
 class MusicProject(info: ProjectInfo) extends DefaultWebProject(info)
 {
   val step = "com.thinkminimo.step" %% "step" % "1.1.6-SNAPSHOT" 
-  val scarla = "net.croz.scardf" % "scardf" % "0.2-SNAPSHOT"
+  val scarla = "net.croz.scardf" %% "scardf" % "0.2-SNAPSHOT"
   val tdb = "com.hp.hpl.jena" % "tdb" % "0.8.5"
-
-  val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
 
   val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.22" % "provided"
   val jettytester = "org.mortbay.jetty" % "jetty-servlet-tester" % "6.1.22" % "test"
-//  val specs = "org.scala-tools.testing" % "specs" % "1.6.2" % "test"
+
   val scalatest = "org.scalatest" % "scalatest" % "1.0" % "test"
 }
 
