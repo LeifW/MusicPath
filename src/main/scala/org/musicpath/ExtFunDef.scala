dc4bc9498709a992fd1391d9baa44d04af53eec3
Leif Warner <abimelech@gmail.com>
2010-08-11 00:03:52 -0700

    Call to XSPARQL compiler on save

--- src/main/scala/org/musicpath/ExtFunDef.scala
+++ src/main/scala/org/musicpath/ExtFunDef.scala
@@ -6,7 +6,7 @@ import net.sf.saxon.value.SequenceType
 import SequenceType.{SINGLE_STRING, SINGLE_ELEMENT_NODE, OPTIONAL_DOCUMENT_NODE}
 
 object ExtFunDef extends ExtensionFunctionDefinition {
-    val getFunctionQName = new StructuredQName("mp", "http://musicpath.org", "sparql")
+    val getFunctionQName = new StructuredQName("mp", "http://musicpath.org/ns/xsparql-fn#", "sparql")
     val getMinimumNumberOfArguments = 1
     //val getMaximumNumberOfArguments = 1
     val getArgumentTypes = List(SINGLE_STRING).toArray
